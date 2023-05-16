8b3e89a15fa4e5ee431af5609ea26c79d9a0409a
Sibil Sarjam Soren <sibil@Sibils-MacBook-Air.local>
2023-05-16 21:36:50 +0530

    day2

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from react"
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is a h1 tag"),
+    React.createElement("h2", {}, "This is a h2 tag"),
+  ])
 );
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello world from react"
+// );
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
