55f295033d457974ce63805925cab4e26abf49fd
Sibil Sarjam Soren <sibil@Sibils-MacBook-Air.local>
2023-05-18 00:57:00 +0530

    New Code

--- App.js
+++ App.js
@@ -1,20 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is a h1 tag1"),
-    React.createElement("h2", {}, "This is a h2 tag2"),
-  ])
-);
+let heading = React.createElement("div", { id: "heading" }, "Namaste React 🚀");
+let root = ReactDOM.createRoot(document.getElementById("root"));
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello world from react"
-// );
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading);
