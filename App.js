b40277776b7c822553b0df94ce7f716803d28554
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2023-05-20 21:36:05 +0530

    React Foundation

--- App.js
+++ App.js
@@ -2,6 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 let heading = React.createElement("div", { id: "heading" }, "Namaste React 🚀");
+
+//React Functional component
+let title = <h3>Yolo</h3>;
+let Title = () => {
+  return <h1 id="heading">This is a heading</h1>;
+};
+let HeadingComponent = () => {
+  return (
+    <h1>
+      {title}
+      <Title />
+      From Functional Component
+    </h1>
+  );
+};
 let root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
